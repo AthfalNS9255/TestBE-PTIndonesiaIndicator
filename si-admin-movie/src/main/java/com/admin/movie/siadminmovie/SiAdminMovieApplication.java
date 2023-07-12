60e94f29e4591d495644750b773d9b04d93ba9e8
Athfal <Athfal@DESKTOP-Q7NDM0F>
2023-07-12 21:02:25 +0700

    Init si-admin-movie

---
+++ si-admin-movie/src/main/java/com/admin/movie/siadminmovie/SiAdminMovieApplication.java
@@ -0,0 +1,13 @@
+package com.admin.movie.siadminmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiAdminMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiAdminMovieApplication.class, args);
+	}
+
+}
---
+++ si-admin-movie/src/test/java/com/admin/movie/siadminmovie/SiAdminMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.movie.siadminmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiAdminMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
