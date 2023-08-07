c44967f5759606b7d43408431e004ee18292e555
Athfal <Athfal@DESKTOP-Q7NDM0F>
2023-08-07 11:04:49 +0700

    Commit

--- siadminmovie/src/main/java/com/siadminmovie/repository/MovieRepository.java
+++ siadminmovie/src/main/java/com/siadminmovie/repository/MovieRepository.java
@@ -3,9 +3,11 @@ package com.siadminmovie.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.siadminmovie.model.Movie;
 
-
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Page<Movie> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
